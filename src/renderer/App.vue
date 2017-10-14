d10bcee222cc91cecb2618007befa15fd8083203
F1shbone <DJ_Side@t-online.de>
2017-10-14 15:34:47 +0200

    Init Commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'yamtgs'
+  }
+</script>
+
+<style>
+  /* SCSS */
+  @import './scss/styles.scss'
+</style>
